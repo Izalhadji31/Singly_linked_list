f69ad1e2c1daf4810ee34e5e7b46ea6d8196078f
Izalhadji31 <115075859+Izalhadji31@users.noreply.github.com>
2022-11-09 16:52:01 +0700

    Menambahkan class List

--- Singly_linked_list/Program.cs
+++ Singly_linked_list/Program.cs
@@ -8,6 +8,13 @@ namespace Singly_linked_list
 {
     class Node
         {
+        public int noMhs;
+        public string nama;
+        public Node next;
         }
+    class List
+    {
+
+    }
     }
 }
